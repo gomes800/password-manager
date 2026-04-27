5e2d67630b926fc273c58052eeba3947f4e2fe42
gomes <joaogomesleite800@gmail.com>
2026-04-27 14:11:20 -0300

    feat: implements encrypt and decrypt functions

--- main.go
+++ main.go
@@ -7,13 +7,28 @@ import (
 )
 
 func main() {
-	password := "aleatoriosenha12345"
+	password := "aleatoriosenha123456"
+	bankPassword := []byte("senhasegura12345@")
 
-	hash, salt, err := security.HashPassword(password)
+	key, salt, err := security.HashPassword(password)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Hash: %x\n", hash)
+	fmt.Printf("Key: %x\n", key)
 	fmt.Printf("Salt: %x\n", salt)
+
+	ciphertext, nonce, err := security.Encrypt(key, bankPassword)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Encrypted: %x\n", ciphertext)
+
+	plaintext, err := security.Decrypt(key, ciphertext, nonce)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Decrypted: %s\n", plaintext)
 }
--- security/hash.go
+++ security/hash.go
@@ -1,7 +1,10 @@
 package security
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
+	"io"
 
 	"golang.org/x/crypto/argon2"
 )
@@ -22,4 +25,44 @@ func HashPassword(password string) ([]byte, []byte, error) {
 	key := argon2.IDKey([]byte(password), salt, time, memory, threads, keyLen)
 
 	return key, salt, nil
+}
+
+func Encrypt(key, plaintext []byte) ([]byte, []byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	nonce := make([]byte, aesGCM.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, nil, err
+	}
+
+	ciphertext := aesGCM.Seal(nil, nonce, plaintext, nil)
+
+	return ciphertext, nonce, nil
+}
+
+func Decrypt(key, ciphertext, nonce []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
 }
